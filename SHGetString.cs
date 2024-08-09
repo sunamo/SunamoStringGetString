cab648ea2ff5b59ad5bf5f11c5ea625c692b013f
sunamo.cz <radek.jancik@sunamo.cz>
2024-08-09 14:40:31 +0200

    Trim XlfKeys

--- SHGetString.cs
+++ SHGetString.cs
@@ -11,33 +11,24 @@ public class SHGetString
         //}
         //return sb.ToString();
 
-        StringBuilder sb = new StringBuilder();
-        foreach (var item in o)
-        {
-            sb.Append(item);
-        }
+        var sb = new StringBuilder();
+        foreach (var item in o) sb.Append(item);
         return sb.ToString();
     }
 
     public static string ListToString(List<string> value, string delimiter = null)
     {
-        if (value == null)
-        {
-            return Consts.nulled;
-        }
+        if (value == null) return Consts.nulled;
 
         string text;
-        Type valueType = value.GetType();
+        var valueType = value.GetType();
 
         if (value is IList && valueType != Types.tString && valueType != Types.tStringBuilder &&
             !(value is IList<char>))
         {
-            if (delimiter == null)
-            {
-                delimiter = Environment.NewLine;
-            }
+            if (delimiter == null) delimiter = Environment.NewLine;
 
-            List<string> enumerable = value; //CA.ToListStringIEnumerable2((IList)value);
+            var enumerable = value; //CA.ToListStringIEnumerable2((IList)value);
             // I dont know why is needed SHReplace.Replace delimiterS(,) for space
             // This setting remove , before RoutedEventArgs etc.
             //CA.SHReplace.Replace(enumerable, delimiterS, AllStrings.space);
